bafc74708c87df674ce5d060cee232f7b3f0bf9c
NEWESTERS <ramiro.cruz@mail.ru>
2016-12-25 22:28:56 +0300

    Added paging in history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def profile
-  	@history = History.all.select{ |x| x.owner == current_user.email }.reverse
+  	@start = params[:start].to_i
+  	history_buf = History.all.select{ |x| x.owner == current_user.email }.reverse
+  	@history = history_buf[@start...@start+10]
+  	@allow_prev = !(history_buf[@start-10...@start] == [])
+  	@allow_next = !history_buf[@start+10...@start+20].nil?
   end
 end
